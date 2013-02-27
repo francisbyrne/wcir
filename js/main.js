187bb557453175bb6d804cc97b30d6b0ea644c1d
byrnify <byrnify@gmail.com>
2013-02-28 07:45:03 +1100

    Added (gross looking) detail table
    Trying to work out the issues with calculations that cause huge number
    increases.

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@
 var wcir = {};
 
 // helper function for getting field values by name
-wcir.getFieldValue = function (fieldName) {
-	var results = $('#wc-form :text[name="' + fieldName + '"]').fieldValue();
-	switch(results.length) {
+wcir.getFieldValue = function ( fieldName ) {
+	var results = $( '#wc-form :text[name="' + fieldName + '"]' ).fieldValue();
+	switch( results.length ) {
 		case 0:
 			return null;
 			break;
 		case 1:
-			return parseInt(results[0]);
+			return parseInt( results[0] );
 			break;
 		default:
 			return results;
@@ -33,47 +33,72 @@ calc.avgDividendYield	= 0.04;
 // returns the number of years it takes for dividend income = annual expenses
 calc.getRetirementYears = function () {
 
-	var netIncome = 0,
-		years 			= 0,
-		capital			= calc.capital,
-		salary 			= calc.salary,
-		expenses 		= calc.expenses;
+	var netIncome = 	0,
+		years 			= 	0,
+		capital 		= 	calc.capital,
+		salary 			= 	calc.salary,
+		expenses 		= 	calc.expenses;
 
-	while (netIncome < expenses) {
+	while ( netIncome < expenses ) {
 		years++;
 		if ( years > 100 )
 			return null;
 
 		capital += salary - expenses;
-		salary  *= Math.pow(1 + this.salaryIncrease, years);
-		expenses *= Math.pow(1 + this.expensesIncrease, years);
+		salary  *= Math.pow( 1 + this.salaryIncrease, years );
+		expenses *= Math.pow( 1 + this.expensesIncrease, years );
 		netIncome = capital * this.avgDividendYield;
+
+		$( '#detail-table' ).append(
+			'<tr><td>' 
+			+ years + '</td><td>' 
+			+ Math.round( capital ) + '</td><td>' 
+			+ Math.round( salary ) + '</td><td>' 
+			+ Math.round( expenses ) + '</td><td>' 
+			+ Math.round( netIncome ) + '</td></tr>'
+		)
 	}
 
-	return years;
+	return {
+		netIncome: Math.round( netIncome ),
+		years:     years,
+		capital:   capital,
+		salary:    salary,
+		expenses:  expenses
+	};
 };
 
 
 
-$(document).ready(function() {
+$(document).ready( function() {
 
 	// hide the results until we have some
-	$('#result').hide();
+	$( '#result' ).hide();
 
 	// ajaxify the form
-	$('#wc-form').ajaxForm(function() {
+	$( '#wc-form' ).ajaxForm(function() {
 
+		$( '#detail-table' ).html(
+			'<tr><th>Year</th><th>Capital</th><th>Salary</th><th>Expense</th><th>Dividend Income</th></tr>'
+		);
 		// get the calculation inputs
-		calc.salary = wcir.getFieldValue('salary');
-		calc.capital = wcir.getFieldValue('capital');
+		calc.salary = wcir.getFieldValue( 'salary' );
+		calc.capital = wcir.getFieldValue( 'capital' );
 
 		// calculate the number of years to retirement
 		var result = calc.getRetirementYears();
-		if (result) 
-			$('#resultValue').html(result);
+		if ( result.years ) 
+			$('#result-years').html( result.years );
+		else
+			$( '#result-years' ).html( '∞' );
+
+		if (result.netIncome) 
+			$( '#result-income' ).html( result.netIncome );
+		else
+			$( '#result-income' ).html( '0' );
 
 		// now display results
-		$('#result').show();
+		$( '#result' ).show();
 
 	});
 })
